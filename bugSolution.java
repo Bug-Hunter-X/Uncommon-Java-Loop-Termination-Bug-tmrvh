c753f3a3acccf0fedcb4469eff8c3f8a5089241f
BugHunter-X <askzenai@gmail.com>
2024-12-29 08:34:04 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,10 @@
+public class MyClass {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            if (i == 5) {
+                break; //Exit loop when condition is met
+            }
+        }
+    }
+}
